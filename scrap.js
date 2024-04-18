1a07d8172dcfafb2325b957613836b3eb7d1236c
MrBaLL <marojoer@gmail.com>
2024-04-18 14:51:44 +0200

    Start

---
+++ scrap.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Codenames Stats
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Sends game data to my codename games stats database
+// @author       MrBall
+// @match        https://codenames.game/*
+// @downloadURL
+// @updateURL
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+})();
